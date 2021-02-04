abeae747957eec99c03a6fa0a976aa2a1ac43f3a
Rafael Borges <borgesrafael.ti@gmail.com>
2021-02-04 20:07:11 -0300

    Adiciona label e classes de alinhamento do Bootstrap no formulário de categoria

--- resources/views/admin/categorias/_partials/form.blade.php
+++ resources/views/admin/categorias/_partials/form.blade.php
@@ -1,9 +1,14 @@
 @csrf
 
-<div class="form-group">
-    <input type="text" name="nome" value="{{ $categoria->nome ?? old('nome') }}" class="form-control <?php echo ($errors->has('nome')) ? 'is-invalid' : '' ?>" placeholder="Nome">
-</div>
+<div class="form-row align-items-center">
 
-<div class="form-group">
-    <input type="submit" value="Salvar" class="btn btn-success">
+    <div class="form-group col-md-12">
+        <label for="nome">Categoria</label>
+        <input type="text" name="nome" value="{{ $categoria->nome ?? old('nome') }}" class="form-control <?php echo ($errors->has('nome')) ? 'is-invalid' : '' ?>" placeholder="Nome">
+    </div>
+
+    <div class="form-group">
+        <input type="submit" value="Salvar" class="btn btn-success">
+    </div>
+    
 </div>
