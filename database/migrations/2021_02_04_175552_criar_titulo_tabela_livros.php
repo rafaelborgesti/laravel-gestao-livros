4fb26724064e2a16ea47cd5ec1d00e3c9111b7ad
Rafael Borges <borgesrafael.ti@gmail.com>
2021-02-04 15:02:32 -0300

    Cria campo título na tabela livros e remove o campo nome

---
+++ database/migrations/2021_02_04_175552_criar_titulo_tabela_livros.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriarTituloTabelaLivros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('livros', function (Blueprint $table) {
+            $table->string('titulo')->after('usuario_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('livros', function (Blueprint $table) {
+            //
+        });
+    }
+}
