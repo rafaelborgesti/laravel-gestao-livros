d8c72e8787e3db7e6c9435b79089a0dcd372c036
Rafael Borges <borgesrafael.ti@gmail.com>
2021-02-04 20:02:05 -0300

    Adiciona a classe css "is-invalid" no formulário de categoria

--- resources/views/admin/categorias/_partials/form.blade.php
+++ resources/views/admin/categorias/_partials/form.blade.php
@@ -1,7 +1,7 @@
 @csrf
 
 <div class="form-group">
-    <input type="text" name="nome" value="{{ $categoria->nome ?? old('nome') }}" class="form-control" placeholder="Nome">
+    <input type="text" name="nome" value="{{ $categoria->nome ?? old('nome') }}" class="form-control <?php echo ($errors->has('nome')) ? 'is-invalid' : '' ?>" placeholder="Nome">
 </div>
 
 <div class="form-group">
